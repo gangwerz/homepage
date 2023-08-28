897f113ffa69a162f0948581fe38292449d5fd20
Zach Gangwer <code@gangwerz.com>
2023-08-28 08:23:13 -0700

    Removed slog

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"os"
+	"fmt"
 	"net/http"
-	"log/slog"
 )
 
 func check(e error) {
@@ -13,41 +12,16 @@ func check(e error) {
 }
 
 func main() {
-	logger := setLogLevel()
-	check(err)
-
-	files, err := os.ReadDir("client")
-	check(err)
-
-	for _, e := range files {
-		slog.Debug("Client File: %s", e.Name())
-	}
-
 	http.HandleFunc("/", root)
 
-	slog.Info("Starting Server on Port 3000...")
+	fmt.Println("Started Server on Port 3000")
 	http.ListenAndServe(":3000", nil)
 }
 
-func setLogLevel() *slog.Logger {
-	lvl, lvl_found := os.LookupEnv("LOG_LEVEL")
-	logger := new(slog.LevelVar)
-
-	if lvl_found {
-		switch lvl {
-			case "DEBUG": logger.Set(slog.LevelDebug)
-			case "WARN": logger.Set(slog.LevelWarn)
-			
-			case "ERROR": logger.Set(slog.LevelError)
-		}
-	}
-
-	return logger
-}
 // --- HANDLERS ---
 
 func root(w http.ResponseWriter, r *http.Request) {
-	slog.Debug("Route Hit:  ROOT")
+	fmt.Println("Route Hit:  ROOT")
 
 	http.ServeFile(w, r, "client/index.html")
 }
