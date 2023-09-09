90858dfbc4a0021f3202ca4ec220ce1d794ddcc3
Zach Gangwer <code@gangwerz.com>
2023-09-09 15:23:35 -0700

    Added ABOUT page

--- main.go
+++ main.go
@@ -8,18 +8,30 @@ import (
     "github.com/julienschmidt/httprouter"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    fmt.Fprint(w, "Welcome!\n")
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w,r, "client/index.html")
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w,r, "client/home.html")
 }
+
+func about(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w,r, "client/about.html")
+}
+
 
 func main() {
+	fmt.Println("HERE")
     router := httprouter.New()
-    router.GET("/", Index)
-    router.GET("/hello/:name", Hello)
+	
+    router.GET("/", index)
+
+	router.GET("/home", home)
+	router.GET("/about", about)
+
+	router.ServeFiles("/static/*filepath", http.Dir("client/static"))
 
     log.Fatal(http.ListenAndServe(":3000", router))
 }
