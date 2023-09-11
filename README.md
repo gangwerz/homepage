7998d41176ea39ab9f2916659f80455f5d3ca982
Zach Gangwer <code@gangwerz.com>
2023-09-10 20:39:57 -0700

    Added README and started work on lorem ipsum generator

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zach Gangwer's Homepage
+
+My homepage written in Go and HTMX.
+
+I will not rule out the use of ECMAscript on this website. However, I am trying
+to minimize my useage of the language.
--- webserv.go
+++ webserv.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "fmt"
     "net/http"
     "log"
 
@@ -10,20 +9,22 @@ import (
 
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Println("HIT -- Index")
 	http.ServeFile(w,r, "client/index.html")
 }
 
 func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Println("HIT -- Home")
 	http.ServeFile(w,r, "client/home.html")
 }
 
 func about(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Println("HIT -- About")
 	http.ServeFile(w,r, "client/about.html")
 }
 
 
 func main() {
-	fmt.Println("HERE")
     router := httprouter.New()
 	
     router.GET("/", index)
@@ -32,7 +33,8 @@ func main() {
 	router.GET("/about", about)
 
 	router.ServeFiles("/static/*filepath", http.Dir("client/static"))
-
+	
+	log.Println("-- Starting Server --")
     log.Fatal(http.ListenAndServe(":3000", router))
 }
 
